784915cc1e13bda7cb24f622499ab1220473d4de
Peter Rudolfsen <peter@silencethefoe.com>
2015-09-02 12:01:28 +0200

    Use LESS programmatically and embed sourcemaps
    Source maps seems to be a bit buggy in Chrome.
    You have to restart the web inspector after changing the CSS,
    and even then it doesn't always work.

--- builders/less.js
+++ builders/less.js
@@ -4,6 +4,8 @@ var shortId = require('shortid');
 var fs = require('fs');
 var chalk = require('chalk');
 var when = require('when');
+var less = require('less');
+var path = require('path');
 
 var stylesheets = process.cwd() + '/assets/stylesheets';
 var compileFileDir = stylesheets;
@@ -25,42 +27,32 @@ module.exports = {
     description: 'LESS files',
 
     build: function (options, done) {
-        var filesToBuild = {};
-        var revisionVar = '@alf-revision:"' + options.revision + '";';
-        var lessc = __dirname + '/../node_modules/.bin/lessc';
-
         glob(stylesheets + '/!(_*).less', {}, function (error, files) {
             files.forEach(function (lessFile) {
                 var cssFile = lessFile.replace('.less', '.css');
-
-                var cmd = [
-                    lessc,
-                    '--global-var="alf-revision=\"' + options.revision + '\""',
-                    lessFile,
-                    cssFile
-                ];
-
-                cp.exec(cmd.join(' '), function (error, stdout, stderr) {
-                    var ok = true;
-
-                    if (error || stderr) {
-                        console.log(chalk.red('There was an error building the files'));
-                        console.log(error);
-                        console.log(stderr);
-
-                        ok = false;
+                var lessOptions = {
+                    filename: lessFile,
+                    sourceMap: {
+                        sourceMapBasepath: stylesheets,
+                        sourceMapFileInline: true
+                    },
+                    globalVars: {
+                        'alf-revision': options.revision
                     }
+                };
 
-                    if (stdout) {
-                        console.log(stdout);
-                    }
+                less.render(fs.readFileSync(lessFile).toString(), lessOptions)
+                    .then(function (output) {
+                        fs.writeFileSync(cssFile, output.css);
 
-                    var fileUrl = cssFile.replace(stylesheets, options.host + '/stylesheets') + '?' + shortId.generate();
+                        var fileUrl = cssFile.replace(stylesheets, options.host + '/stylesheets') + '?' + shortId.generate();
 
-                    if (ok) {
                         done([fileUrl]);
-                    }
-                });
+                    }).
+                    catch(function (error) {
+                        console.log(chalk.red.bold(error));
+                        console.log(chalk.yellow(JSON.stringify(error, null, 4)));
+                    })
             });
         });
     }
